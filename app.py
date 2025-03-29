9106763dbcb9b86a899643d84e047ed09fc7d76f
Ayush-e4 <ayush209.k@gmail.com>
2025-03-30 02:22:42 +0530

    Added dynamic rendering and jsonify routes

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Banglore',
+        'salary': 10000
+    },
+    {
+        'id': 2,
+        'title': 'Frontend Engg.',
+        'location': 'Delhi'
+    },
+    {
+        'id': 3,
+        'title': 'Backend Engg.',
+        'location': 'Kolkata',
+        'salary': 56000
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "Hello World, Sup Fellas!"
+    return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
